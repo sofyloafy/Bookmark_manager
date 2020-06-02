ef4eb9e5ac903c10118461753fba5f5efa6768bb
Sophie <sophiebrown438@gmail.com>
2020-06-02 14:34:58 +0100

    Passing test for bookmark unordered list

--- app.rb
+++ app.rb
@@ -3,7 +3,14 @@ require 'sinatra/base'
 class BookmarkManager < Sinatra::Base
 
   get '/' do
-    "Welcome to Bookmark Manager!"
+    "Welcome!"
+  end
+
+  get '/bookmarks' do
+    @bookmarks = ['https://github.com/makersacademy/course/blob/master/bookmark_manager/03_viewing_bookmarks',
+                  'https://www.bbc.co.uk/'
+                ]
+    erb :bookmarks
   end
 
   run! if app_file == $0
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 
 feature 'Bookmark Manager' do
-  scenario "it should produce bookmark homepage" do
+  scenario "it should redirect to bookmarks" do
     visit '/'
-    expect(page).to have_content 'Welcome to Bookmark Manager!'
+    expect(page).to have_content 'Welcome!'
+  end
+
+
+  scenario "it should produce bookmarks" do
+    visit '/bookmarks'
+    expect(page).to have_content 'https://github.com/makersacademy/course/blob/master/bookmark_manager/03_viewing_bookmarks'
+    expect(page).to have_content 'https://www.bbc.co.uk/'
   end
 end
