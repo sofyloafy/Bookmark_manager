8de9421a4fe1fb30b072a7831f291e07cb1f1102
Sophie <sophiebrown438@gmail.com>
2020-06-05 14:59:25 +0100

    Passing persisted data test

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,4 +1,6 @@
 require 'bookmark'
+require 'database_helpers'
+
 
 describe Bookmark do
   describe '.all' do
@@ -17,3 +19,16 @@ describe Bookmark do
     end
   end
 end
+
+describe '.create' do
+  it 'creates a new bookmark' do
+    bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+    persisted_data = persisted_data(id: bookmark.id)
+
+    expect(bookmark).to be_a Bookmark
+    expect(bookmark.id).to eq persisted_data['id']
+    expect(bookmark.title).to eq 'Test Bookmark'
+    expect(bookmark.url).to eq 'http://www.testbookmark.com'
+  end
+end
+
